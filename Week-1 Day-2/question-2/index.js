8e26177fe54372ab4b048036d841b9ca65c2d240
suprajagangarajula7 <suprajagangarajula7@gmail.com>
2026-01-13 15:03:06 +0530

    Q2

---
+++ Week-1 Day-2/question-2/index.js
@@ -0,0 +1,7 @@
+import { checkPrime } from "./math.js";
+
+console.log(checkPrime(2));   
+console.log(checkPrime(7));   
+console.log(checkPrime(10));  
+console.log(checkPrime(1));   
+console.log(checkPrime(17));  
---
+++ Week-1 Day-2/question-2/math.js
@@ -0,0 +1,12 @@
+export function checkPrime(number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
